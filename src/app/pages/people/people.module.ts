b691a3be761d06d40c4610f42b6f30bc5d94c14a
navahm <navahm@microsoft.com>
2018-02-27 17:27:20 -0500

    person details

--- src/app/pages/people/people.module.ts
+++ src/app/pages/people/people.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 
 import { ThemeModule } from '../../@theme/theme.module';
 import { PeopleRoutingModule, routedComponents } from './people-routing.module';
+import { PersonDetailsComponent } from './person/person-details/person-details.component';
 
 @NgModule({
   imports: [
@@ -10,6 +11,7 @@ import { PeopleRoutingModule, routedComponents } from './people-routing.module';
   ],
   declarations: [
     ...routedComponents,
+    PersonDetailsComponent,
   ],
 })
 export class PeopleModule { }
