152af70a1f9829d56c1ef34204e054c751c29bef
Naveed Ahmad <nAzeemi@hotmail.com>
2018-05-25 19:56:56 -0400

    Adding DOB and Address

--- src/app/pages/people/person/person.component.ts
+++ src/app/pages/people/person/person.component.ts
@@ -16,17 +16,31 @@ export class PersonComponent {
 
   searchPerson() {
     this.showNoneFound = false;
-    this.showNewPerson = false;
+    this.showNewPerson = false; 
     this.searchResults = [
       {
         Id: 'guid1',
-        FirstName: 'Naveed',
-        LastName: 'Ahmad',
+        Name: 'Naveed Ahmad',
+        DateOfBirth: new Date('1972-07-20'),
+        Address: {
+          Street1: '39 Maurice St',
+          Street2: '',
+          City: 'Medford',
+          State: 'MA',
+          Zip: '02155',
+        }
       },
       {
         Id: 'guid2',
-        FirstName: 'Saima',
-        LastName: 'Ahmad',
+        Name: 'Saima Ahmad',
+        DateOfBirth: new Date('1975-01-02'),
+        Address: {
+          Street1: '39 Maurice St',
+          Street2: '2nd Floor',
+          City: 'Medford',
+          State: 'MA',
+          Zip: '02155',
+        }
       },
     ];
     this.showNoneFound = this.searchResults.length === 0
